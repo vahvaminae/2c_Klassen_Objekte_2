5f4dff67cea15aa7a33005638e1111e22c487571
Jenny <JenifferLeutert@gmx.de>
2023-07-28 10:24:00 +0200

    Name zu firstName verändert

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,13 +2,13 @@ package com.cc.java;
 
 public class Cat {
 
-  private String name;
+  private String firstName;
   private String furColor;
   private int age; 
 
 
-public Cat(String name, String furColor, int age) {
-    this.name = name;
+public Cat(String firstName, String furColor, int age) {
+    this.firstName = firstName;
     this.furColor = furColor;
     this.age = age;
   }
@@ -16,7 +16,7 @@ public Cat(String name, String furColor, int age) {
 public String getStringAttributes(String op) {
   switch (op) {
     case "#name":
-      return name;
+      return firstName;
     case "#color":
       return furColor;
     default:
